def406fde704697265432679ae641d58938a1189
peternicola <peter_nicola15@hotmail.com>
2019-11-14 12:17:14 +0200

    Update feedback.js

--- docs/feedback.js
+++ docs/feedback.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://d526c02f.eu-gb.apiconnect.appdomain.cloud/07-feedback';
+const apiUrl = 'https://d526c02f.eu-gb.apiconnect.appdomain.cloud/119-feedback';
 const feedback = {
   // retrieve the existing feedback entries
   get() {
